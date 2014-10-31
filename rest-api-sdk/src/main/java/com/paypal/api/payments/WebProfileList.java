83db5d47bdf3e7fddc1e4346f5fad2549600edc9
juwon <juwlee@paypal.com>
2014-10-31 10:33:29 -0500

    Add webprofile

---
+++ rest-api-sdk/src/main/java/com/paypal/api/payments/WebProfileList.java
@@ -0,0 +1,51 @@
+package com.paypal.api.payments;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.paypal.core.rest.JSONFormatter;
+import com.paypal.api.payments.FlowConfig;
+import com.paypal.api.payments.InputFields;
+import com.paypal.api.payments.Presentation;
+
+public class WebProfileList extends ArrayList<WebProfile> {
+
+	private List<WebProfile> webProfiles = null;
+	
+	/**
+	 * Default Constructor
+	 */
+	public WebProfileList() {
+		webProfiles = new ArrayList<WebProfile>();
+	}
+
+
+	/**
+	 * Setter for items
+	 */
+	public List<WebProfile> setWebProfileList(List<WebProfile> webProfiles) {
+		this.webProfiles = webProfiles;
+		return this.webProfiles;
+	}
+
+	/**
+	 * Getter for items
+	 */
+	public List<WebProfile> getWebProfileLists() {
+		return this.webProfiles;
+	}
+	
+	/**
+	 * Returns a JSON string corresponding to object state
+	 *
+	 * @return JSON representation
+	 */
+	public String toJSON() {
+		return JSONFormatter.toJSON(this);
+	}
+
+	@Override
+	public String toString() {
+		return toJSON();
+	}
+}
