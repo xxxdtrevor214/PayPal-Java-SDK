7bf65ea5970340ed6b9ba78677175c16efba7e38
juwon <juwlee@paypal.com>
2014-11-25 14:50:19 -0600

    Remove unnecessary tests

--- rest-api-sdk/src/test/java/com/paypal/api/payments/IntegrationTest.java
+++
@@ -1,9 +0,0 @@
-package com.paypal.api.payments;
-
-/**
- * A functional test that hits external APIs over the wire (usually PayPal sandbox environment). 
- * Depending on the external API status, this test may not show expected behavior.
- */
-public interface IntegrationTest {
-
-}
