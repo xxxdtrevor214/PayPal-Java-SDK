34e930d9dc251b7f58580c4ba37c4fa56b5392d2
japatel <japatel@paypal.com>
2015-01-20 11:21:48 -0600

    Updated Client Credentials to use similar credentials from other SDKs.

--- rest-api-sdk/src/test/java/com/paypal/api/payments/WebhooksInputData.java
+++ rest-api-sdk/src/test/java/com/paypal/api/payments/WebhooksInputData.java
@@ -9,17 +9,17 @@ public class WebhooksInputData {
 		{"PAYMENT.CAPTURE.REFUNDED", "A capture payment was refunded"},
         {"PAYMENT.AUTHORIZATION.VOIDED", "A payment authorization was voided"},
         {"PAYMENT.AUTHORIZATION.CREATED", "A payment authorization was created"},
-        {"PAYMENT.SALE.COMPLETED", "A sale payment was completed"},	                
-        {"PAYMENT.SALE.REFUNDED", "A sale payment was refunded"},	                
+        {"PAYMENT.SALE.COMPLETED", "A sale payment was completed"},
+        {"PAYMENT.SALE.REFUNDED", "A sale payment was refunded"},
         {"PAYMENT.SALE.REVERSED", "A sale payment was reversed"},
         {"PAYMENT.CAPTURE.REVERSED", "A capture payment was reversed"}
 	};
-	
+
 	public static final String WEBHOOK_URL = "https://github.com/paypal/";
 	public static final String WEBHOOK_HATEOAS_URL = "https://api.sandbox.paypal.com/v1/notifications/webhooks/";
 
-	public static final String CLIENT_ID = "EBWKjlELKMYqRNQ6sYvFo64FtaRLRR5BdHEESmha49TM";
-	public static final String CLIENT_SECRET = "EO422dn3gQLgDbuwqTjzrFgFtaRLRR5BdHEESmha49TM";
+	public static final String CLIENT_ID = "AYSq3RDGsmBLJE-otTkBtM-jBRd1TCQwFf9RGfwddNXWz0uFU9ztymylOhRS";
+	public static final String CLIENT_SECRET = "EGnHDxD_qRPdaLdZz8iCr8N7_MzF-YHPTkjs6NKYQvQSBngp4PTTVWkPZRbL";
 	public static final String EVENT_ID = "WH-SDK-1S115631EN580315E-9KH94552VF7913711";
 
 	public static final String LINK_HREF = "https://api.sandbox.paypal.com/v1/notifications/webhooks-events/" + EVENT_ID;
@@ -28,7 +28,7 @@ public class WebhooksInputData {
 	public static final String LINK_REL_RESEND = "resend";
 	public static final String LINK_METHOD_GET = "GET";
 	public static final String LINK_METHOD_POST = "POST";
-	
+
 	/**
 	 * Static method top generate List of Links
 	 */
@@ -46,5 +46,5 @@ public class WebhooksInputData {
 		linksList.add(link2);
 		return linksList;
 	}
-	
+
 }
