3bcfa8575b5d472de983c0d0cbd28620711764d8
juwon <juwlee@paypal.com>
2015-10-28 09:33:23 -0500

    change SSL protocol to TLS 1.2

--- rest-api-sdk/src/main/java/com/paypal/base/SSLUtil.java
+++ rest-api-sdk/src/main/java/com/paypal/base/SSLUtil.java
@@ -80,7 +80,11 @@ public abstract class SSLUtil {
 	public static SSLContext getSSLContext(KeyManager[] keymanagers)
 			throws SSLConfigurationException {
 		try {
-			SSLContext ctx = SSLContext.getInstance(CONFIG_MAP.get(Constants.SSLUTIL_PROTOCOL)); // TLS, SSLv3, SSL
+			String protocol = CONFIG_MAP.get(Constants.SSLUTIL_PROTOCOL);
+			if (protocol == null || protocol.equals("") || protocol.startsWith("SSL") || (protocol.startsWith("TLS") && !protocol.equals("TLSv1.2"))) {
+				protocol = "TLSv1.2";
+			}
+			SSLContext ctx = SSLContext.getInstance(protocol);
 			ctx.init(keymanagers, null, null);
 			return ctx;
 		} catch (Exception e) {
