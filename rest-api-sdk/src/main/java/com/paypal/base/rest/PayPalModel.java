1993efda27c59d8718696b1d6a92b59bcc403c06
juwon <juwlee@paypal.com>
2015-11-04 08:06:06 -0600

    fix json comparison in PayPalModel (#135)

--- rest-api-sdk/src/main/java/com/paypal/base/rest/PayPalModel.java
+++ rest-api-sdk/src/main/java/com/paypal/base/rest/PayPalModel.java
@@ -18,11 +18,15 @@ public class PayPalModel {
 	
 	@Override
 	public boolean equals(Object obj) {
-		if (obj == null) return false;
-	    if (obj == this) return true;
-	    if (!(obj instanceof PayPalModel))return false;
+		if (obj == null) 
+			return false;
+	    if (obj == this) 
+	    	return true;
+	    if (!(obj instanceof PayPalModel))
+	    	return false;
 	    PayPalModel objClass = (PayPalModel)obj;
-	    if (objClass.toJSON() == toJSON()) return true;
+	    if (objClass.toJSON().equals(toJSON())) 
+	    	return true;
 	    return false;
 	}
 	
