xxxdtrevor214/PayPal-Java-SDK

commit 110c76f22e6fa74fa70acdddaf87425e568edcbb

Created new static getCredential method
- Covers backward compatibility issues.

rest-api-sdk/src/main/java/com/paypal/base/rest/PayPalResource.java
@@ -496,9 +496,8 @@ public abstract class PayPalResource extends PayPalModel{
 	 * Returns ClientCredentials with client id and client secret from configuration Map
 	 *
 	 * @return Client credentials
-	 * @throws IOException 
      */
-	public ClientCredentials getClientCredential() throws IOException {
+	public static ClientCredentials getCredential() {
 		ClientCredentials credentials = new ClientCredentials();
 		Properties configFileProperties = getConfigFileProperties();
 		mergeProperties(configFileProperties, configurationMap);
@@ -507,18 +506,42 @@ public abstract class PayPalResource extends PayPalModel{
 		return credentials;
 	}
 
-	private Properties getConfigFileProperties() throws IOException {
+	/**
+	 * @deprecated Please use static method `getCredential` instead.
+	 *
+	 * Returns ClientCredentials with client id and client secret from configuration Map.
+	 *
+	 * @return Client credentials
+	 */
+	public ClientCredentials getClientCredential() {
+		return PayPalResource.getCredential();
+	}
+	
+	/**
+	 * Fetches the properties from default configuration file.
+	 * 
+	 * @return {@link Properties}
+	 */
+	private static Properties getConfigFileProperties() {
 		Properties properties = new Properties();
 		try {
 			properties.load(new FileReader(
-					new File(getClass().getClassLoader().getResource(Constants.DEFAULT_CONFIGURATION_FILE).getFile())));
+					new File(PayPalResource.class.getClassLoader().getResource(Constants.DEFAULT_CONFIGURATION_FILE).getFile())));
 		} catch (FileNotFoundException e) {
+			return null;
+		} catch (IOException e) {
 			return null;
 		}
 		return properties;
 	}
 
-	private void mergeProperties(Properties properties, Map<String, String> configurationMap) {
+	/**
+	 * Merges properties object with the configuration hash map. The configuration values are given higher priority.
+	 * 
+	 * @param properties
+	 * @param configurationMap
+	 */
+	private static void mergeProperties(Properties properties, Map<String, String> configurationMap) {
 		if (properties != null) {
 			for (final String name : properties.stringPropertyNames()) {
 				if (!configurationMap.containsKey(name)) {

rest-api-sample/src/main/java/com/paypal/api/sample/FuturePaymentSample.java
@@ -48,7 +48,7 @@ public class FuturePaymentSample {
 		if (authorizationCode != null && authorizationCode.trim().length() > 0) {
 			log.info("creating future payment with auth code: " + authorizationCode);
 			
-			ClientCredentials credentials = futurePayment.getClientCredential();
+			ClientCredentials credentials = PayPalResource.getCredential();
 			CreateFromAuthorizationCodeParameters params = new CreateFromAuthorizationCodeParameters();
 			params.setClientID(credentials.getClientID());
 			params.setClientSecret(credentials.getClientSecret());

rest-api-sample/src/main/java/com/paypal/api/sample/ThirdPartyInvoice.java
@@ -47,7 +47,7 @@ public class ThirdPartyInvoice extends SampleBase<Invoice> {
 
 		// Setup the refresh token params. This will be used to get access token
 		// from refresh token
-		ClientCredentials credentials = super.instance.getClientCredential();
+		ClientCredentials credentials = PayPalResource.getCredential();
 		CreateFromRefreshTokenParameters params = new CreateFromRefreshTokenParameters();
 		params.setClientID(credentials.getClientID());
 		params.setClientSecret(credentials.getClientSecret());

rest-api-sdk/src/test/java/com/paypal/base/rest/PayPalResourceTestCase.java
@@ -6,7 +6,6 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
-import com.paypal.api.payments.Payment;
 import com.paypal.base.ClientCredentials;
 import com.paypal.base.Constants;
 import org.testng.Assert;
@@ -65,7 +64,7 @@ public class PayPalResourceTestCase {
 			PayPalResource.initConfig(props);
 
 			// Check if ClientCredentials is constructed correctly
-			ClientCredentials clientCredentials = new Payment().getClientCredential();
+			ClientCredentials clientCredentials = PayPalResource.getCredential();
 			Assert.assertEquals(props.getProperty(Constants.CLIENT_ID), clientCredentials.getClientID());
 			Assert.assertEquals(props.getProperty(Constants.CLIENT_SECRET), clientCredentials.getClientSecret());
 		} catch (FileNotFoundException e) {

commit 327c78b775aba9237e9bbbdb4a203500e8bc9d07

Fixes Merge Properties
- Bug introduced in #189

rest-api-sdk/src/main/java/com/paypal/base/rest/PayPalResource.java
@@ -499,7 +499,7 @@ public abstract class PayPalResource extends PayPalModel{
 	public static ClientCredentials getCredential() {
 		ClientCredentials credentials = new ClientCredentials();
 		Properties configFileProperties = getConfigFileProperties();
-		mergeProperties(configFileProperties, configurationMap);
+		addConfigurations(configFileProperties);
 		credentials.setClientID(configurationMap.get(Constants.CLIENT_ID));
 		credentials.setClientSecret(configurationMap.get(Constants.CLIENT_SECRET));
 		return credentials;
@@ -538,9 +538,11 @@ public abstract class PayPalResource extends PayPalModel{
 	 * Merges properties object with the configuration hash map. The configuration values are given higher priority.
 	 * 
 	 * @param properties
-	 * @param configurationMap
 	 */
-	private static void mergeProperties(Properties properties, Map<String, String> configurationMap) {
+	private static void addConfigurations(Properties properties) {
+		if (configurationMap == null) {
+			configurationMap = new HashMap<String, String>();
+		}
 		if (properties != null) {
 			for (final String name : properties.stringPropertyNames()) {
 				if (!configurationMap.containsKey(name)) {
@@ -549,5 +551,4 @@ public abstract class PayPalResource extends PayPalModel{
 			}	
 		}
 	}
-
 }