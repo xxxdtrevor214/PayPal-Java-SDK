98948399198f8fd9d7c9ca82417cfafa183f52e2
demerino and sdcoffey <david.eros.merino@gmail.com and sdcoffey@umich.edu>
2017-01-27 14:15:48 -0800

    Cleanup PayPalHttpClient using internal injectors

--- core/src/main/java/com/paypal/sdk/PayPalHttpClient.java
+++ core/src/main/java/com/paypal/sdk/PayPalHttpClient.java
@@ -13,17 +13,17 @@ import static com.paypal.sdk.http.Headers.CONTENT_TYPE;
 @Slf4j
 public class PayPalHttpClient extends DefaultHttpClient implements HttpClient {
 
-	private Injector mAuthInjector;
     private Environment mEnvironment;
 
     public PayPalHttpClient(Environment environment) {
-    	mAuthInjector = new OAuthInjector(environment);
-		mEnvironment = environment;
+    	this(new OAuthInjector(environment), environment);
     }
 
     public PayPalHttpClient(Injector authInjector, Environment environment) {
-    	mAuthInjector = authInjector;
-    	mEnvironment = environment;
+    	super();
+		mEnvironment = environment;
+    	addInjector(authInjector);
+    	addInjector(new PayPalDefaultInjector());
 	}
 
 	@Override
@@ -31,29 +31,20 @@ public class PayPalHttpClient extends DefaultHttpClient implements HttpClient {
     	return "Java PayPalHttpClient"; // TODO: Return SDK version as part of this user agent.
 	}
 
-	/**
-	 * Make an HTTP request to the base url using the parameters specified in the {@link HttpRequest} object.
-	 *
-	 * @param request the {@link HttpRequest} object containing the parameters for this request
-	 * @return an {@link HttpResponse} object containing the parameters from a successful call to the server
-	 * @throws IOException
-	 */
-	@Override
-    public <T> HttpResponse<T> execute(HttpRequest<T> request) throws IOException {
-		if (mAuthInjector != null) {
-			mAuthInjector.inject(request);
+	private class PayPalDefaultInjector implements Injector {
+
+		@Override
+		public <T> void inject(HttpRequest<T> request) throws IOException {
+			if (request.baseUrl() == null) {
+				request.baseUrl(mEnvironment.baseUrl());
+			}
+
+			if (request.requestBody() != null) {
+				request.headers().headerIfNotPresent(CONTENT_TYPE, "application/json");
+			}
+
+			request.headers().headerIfNotPresent(ACCEPT_ENCODING, "gzip");
+
 		}
-
-		if (request.baseUrl() == null) {
-			request.baseUrl(mEnvironment.baseUrl());
-		}
-
-		if (request.requestBody() != null) {
-			request.headers().headerIfNotPresent(CONTENT_TYPE, "application/json");
-		}
-
-		request.headers().headerIfNotPresent(ACCEPT_ENCODING, "gzip");
-
-		return super.execute(request);
 	}
 }
