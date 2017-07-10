52de3f5cbb5e23f38758a9a277ad6a37ff4d78d8
sashaweiss and sdcoffey <code@getbraintree.com>
2017-07-10 16:15:31 -0700

    Update Agent Header in PayPalHttpClient

--- paypal-sdk/src/main/java/com/paypal/core/PayPalHttpClient.java
+++ paypal-sdk/src/main/java/com/paypal/core/PayPalHttpClient.java
@@ -4,6 +4,7 @@ import com.braintreepayments.http.Headers;
 import com.braintreepayments.http.HttpClient;
 import com.braintreepayments.http.HttpRequest;
 import com.google.gson.Gson;
+import com.paypal.BuildConfig;
 import com.paypal.core.authorization.PayPalEnvironment;
 import com.paypal.core.object.AccessToken;
 import com.paypal.core.request.AccessTokenRequest;
@@ -19,6 +20,8 @@ public class PayPalHttpClient extends HttpClient {
 	// Visible for testing
 	protected AccessToken accessToken;
 
+	private static final String USER_AGENT;
+
 	public PayPalHttpClient(PayPalEnvironment environment) {
 		super(environment);
 		this.gson = new Gson();
@@ -33,7 +36,7 @@ public class PayPalHttpClient extends HttpClient {
 
 	@Override
 	protected String getUserAgent() {
-		return "Java PayPalHttpClient"; // TODO: Return SDK version as part of this user agent.
+		return USER_AGENT;
 	}
 
 	@Override
@@ -82,5 +85,51 @@ public class PayPalHttpClient extends HttpClient {
 	private boolean isContentTypeJson(Headers headers) {
 		String contentType = headers.header(Headers.CONTENT_TYPE);
 		return contentType != null && contentType.equals("application/json");
+	}
+
+	static {
+		StringBuilder javaVersion = new StringBuilder();
+		if (System.getProperty("java.version") != null
+				&& System.getProperty("java.version").length() > 0) {
+			javaVersion.append("v=")
+					.append(System.getProperty("java.version"));
+		}
+		if (System.getProperty("java.vendor") != null
+				&& System.getProperty("java.vendor").length() > 0) {
+			javaVersion.append("; vendor=" + System.getProperty("java.vendor"));
+		}
+		if (System.getProperty("java.vm.name") != null
+				&& System.getProperty("java.vm.name").length() > 0) {
+			javaVersion.append("; bit=");
+			if (System.getProperty("java.vm.name").contains("Client")) {
+				javaVersion.append("32");
+			} else {
+				javaVersion.append("64");
+			}
+		}
+
+		StringBuilder osVersion = new StringBuilder();
+		if (System.getProperty("os.name") != null
+				&& System.getProperty("os.name").length() > 0) {
+			osVersion.append("os=");
+			osVersion.append(System.getProperty("os.name").replace(' ', '_'));
+		} else {
+			osVersion.append("os=");
+		}
+		if (System.getProperty("os.version") != null
+				&& System.getProperty("os.version").length() > 0) {
+			osVersion.append(" "
+					+ System.getProperty("os.version").replace(' ', '_'));
+		}
+
+		StringBuilder stringBuilder = new StringBuilder("PayPalSDK/"
+				+ "PayPal-Java-SDK" + " " + BuildConfig.VERSION + " ");
+		stringBuilder.append("(").append(javaVersion.toString());
+		if (osVersion.length() > 0) {
+			stringBuilder.append("; ").append(osVersion.toString());
+		}
+		stringBuilder.append(")");
+
+		USER_AGENT = stringBuilder.toString();
 	}
 }
