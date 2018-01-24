03e115f7fc59f4b0feef79243f1e119a2d7aa904
sdcoffey <sdcoffey@umich.edu>
2018-01-23 16:23:51 -0800

    Add payment experience API

---
+++ paypal-sdk/src/main/java/com/paypal/sdk/paymentexperience/WebProfile.java
@@ -0,0 +1,101 @@
+// This class was generated on Tue, 23 Jan 2018 14:59:52 PST by version 0.1.0-dev+c58b80 of Braintree SDK Generator
+// WebProfile.java
+// @version 0.1.0-dev+c58b80
+// @type object
+// @data H4sIAAAAAAAC/8xYX3PbuBF/76fY0cu1M4riXKft1DN9cOLEcZM6Glu5PLQ31IpckXsBAR4AWqY7/e6dBUhJFKXYvthuHwxT2MVif/sXwL9Hs6ai0fHoCy1gas2SFY3Go5/QMi4UXWApxNF49IGazY9TcqnlyrPRo+PRCVTYlKQ9rGgBdFORZdIpQRXlTUbj0Ym12MStjsajS8Lsk1bN6HiJypFM/FqzpWw9MbWmIuuZ3Oj4n2sl3ymzgjdGLzkfKrlUZpWkHXGjb3++r/qsIBA6RHptUQhQocWSPFn3eLo7b1nvUXuB+mvib3RSkc5Y50ltVU//AwxDICXZtEDtwbEn+Hz5EbyBjF2lsAFcerKAINLAW9RuSbZz3ATONZyRLVE3Y7hGxRkYrRpYGgu+IDhjayps1o4uyRcmA2P3y2vp32s8XSv1n/GdFlQY7VJhTokX3m3z7aMObScEMEtouUG4t81ndLDDFJspqmhgsU3tyEJaUPrV1H4CMyMzgVMb/aLlxjQ1tfY92WNw5GWD+WtWinU+763eXWly1gfWfxTa/JlMbcnXVie15aTwvkqin3v2PsgyNPr72WzahZJvwRuwlLGl1AdDpLXzpiQrBIQoW0J7Aj+hqgnYwfzs7WwuoTiffrqaPZclxPMJpgHNNvz+fB/z1JIj7R0Q+4JiZv2rPjr6Y+q8NToP3/TGaM+6pkh5uU0SlMMFU2zgwqz28XehGYucNz2bTuKKhX0ZuXd+ntISa+XFxA9ScrzRMjVZ5KAts/wt7a/dME3gS0EaGlNDZkAbD1+1qC1FmjWqdXnBUtJiLPlBlQQKO8hadQXpGFYFp8U6lNwglraSrBMaUn7FvvgNflnU3hvd6t/bK1Wcfn2wBfsiUIMU9jzWhr4R7nDixqJ3mFLKybedVpbsBy4TW6aFMW3delBwPpmnvrHnb3DUYWnjnj/YyYEnJecoAy5Lyhg9qeZRCtLP9yhJ3C/EvL/onp9KnxPFn+a0tlM/z3VVe3jHpDK3R2WhJsuOuqV8nzCEERggMLQ+5NunO7yx9pSTHQLALLPkXGKuyVrO+kePPcQ+kHOdcYqeHKwKii1hc+QQF7mCq0p6fisKfIEx0jCEWcgPdpCiUmCx7SoYQ9to+QNxanuQYweLuiG7c5zZDgLMyW2a63GM+lptFxfF2782teBoT0E/jWDcfjStdm0Ne9kXfGibVw/exlWU8pIlLfXGXpNBlZWOQxm3DWVXzH4tX3a2edy0WRijCPWegFNyndHG74Ta9vSdQbbbXUjHy4Gs72ru+jbRj5VQe7NaKtDm4Pu42A8mmzZJ55Qe+P78XehbIFkXMoNwiVWnw/2/TI4du7vvT5RLylC67CHQS2vK4bbwzljIOGePCnJjMjeOORLrLzuwrX8BdQZl7TwsCO6h0IMA/bgH0Bn5A/ZbQ+lC/Qe3vlB1ve7pU/o+jVvLv15Ax4lh19vfskH4J/CP1uy15l/rWPIxnOvMsuN07WyX3A/KXG/rbydue7/B9vKzg7LqUzdodwhD1M43ikJkbbM+7yuNRZ0lA0f1pndfxRQuSMWO3Z0AYqguasdaQlQWxp40uPLvy354U6DFVGq1Ip37IthEccnRIu4YXv34F3Csc0UvFo0nQFUVqOuSLKdyiYjL3XO9zJgUFSWSr/03md780OGRHuJWgEcb7rkIbKVCV84ncNI+YF2vHwhOPs/HMD+Zyfj6bRgvZXxzEsb3YbyQ8TRQ317JePZaxneB8zysvfgo4zSOYeZSJBsL889Xc9n5Ty/WNgZBJ9ujcqZVSZJPoc7rgCo63hEEj1smdwzzDJPTDyK6oOQ87MRZcn4qH79g8vepfGiTXHySj8onEYmtk6DK3F0nVwGDL5JZAHZbJBHbbZG8/9Cqe1sksy/zZ4uC3CRcYr4bBFvTu7nTvl76EAy5gcC38W24XMVHO3HwJOelQJz8UuUtwkkV39TkqiBrf3BQ4g2XdQkrznwh61799QgqviHlYttq6QVxXoRT9p87+qSLvNSaykWJbViiJVBoc7JrpkphSk5u3XZLe2hj2Juqu4GlaD1YumZaiRxcS7Ak923SWbuJXOCdNzbesqM4o0N5T2tL8Pv3s9n06g/gyF6LIp/kpLNiR+PQMhbWrBxZt3kNhpKcC2+cIn79TBTTLTXaI+vwhtluwJ7K/7sC9PN9DsyeyspYtE0v+LZn73Nc7jptRdax8669UFkiKExtncRcFR7QSXvVhMfclnd3fcezecIN+OYH1+xstVknDXn+SJb83X8BAAD//w==
+// DO NOT EDIT
+package com.paypal.sdk.paymentexperience;
+
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
+import com.braintreepayments.http.annotations.*;
+/**
+ * A payment web experience profile.
+ */
+@Model
+public class WebProfile {
+
+    // Required default constructor
+    public WebProfile() {}
+
+	/**
+	* The flow configuration parameters.
+	*/
+	@SerializedName("flow_config")
+	private FlowConfig flowConfig;
+
+	public FlowConfig flowConfig() { return flowConfig; }
+	
+	public WebProfile flowConfig(FlowConfig flowConfig) {
+	    this.flowConfig = flowConfig;
+	    return this;
+	}
+
+	/**
+	* The ID of the web experience profile.
+	*/
+	@SerializedName("id")
+	private String id;
+
+	public String id() { return id; }
+	
+	public WebProfile id(String id) {
+	    this.id = id;
+	    return this;
+	}
+
+	/**
+	* The input field customization parameters.
+	*/
+	@SerializedName("input_fields")
+	private InputFields inputFields;
+
+	public InputFields inputFields() { return inputFields; }
+	
+	public WebProfile inputFields(InputFields inputFields) {
+	    this.inputFields = inputFields;
+	    return this;
+	}
+
+	/**
+	* REQUIRED
+	* The web experience profile name. Must be unique for a set of profiles for a merchant.
+	*/
+	@SerializedName("name")
+	private String name;
+
+	public String name() { return name; }
+	
+	public WebProfile name(String name) {
+	    this.name = name;
+	    return this;
+	}
+
+	/**
+	* The style and presentation parameters.
+	*/
+	@SerializedName("presentation")
+	private Presentation presentation;
+
+	public Presentation presentation() { return presentation; }
+	
+	public WebProfile presentation(Presentation presentation) {
+	    this.presentation = presentation;
+	    return this;
+	}
+
+	/**
+	* Indicates whether the profile persists for three hours or permanently. To persist the profile permanently, set to `false`. To persist the profile for three hours, set to `true`.
+	*/
+	@SerializedName("temporary")
+	private Boolean temporary;
+
+	public Boolean temporary() { return temporary; }
+	
+	public WebProfile temporary(Boolean temporary) {
+	    this.temporary = temporary;
+	    return this;
+	}
+}
---
+++ paypal-sdk/src/main/java/com/paypal/sdk/paymentexperience/WebProfilePartialUpdateRequest.java
@@ -0,0 +1,33 @@
+// This class was generated on Tue, 23 Jan 2018 14:59:52 PST by version 0.1.0-dev+c58b80 of Braintree SDK Generator
+// WebProfilePartialUpdateRequest.java
+// @version 0.1.0-dev+c58b80
+// @type request
+// @data H4sIAAAAAAAC/7SVUU/bQAzH3/cpLD8fLZumPeQNwSbYNOgG2suEwE1ccii9O3wOJUL97tMlaUtTNMamvbX/cxz//Hd8j3hKc8YMFzzdC+JntuJRIFFL1V4dClJGg0ccc7FBrXeY4aQ7rpo+IALBgqfAD4HFsssZ+kwGpg2cHI3gxIGWDJ/Pz05B+K7mqDD1RWMgBs7trAGCQJqX4Ke3nKtpwwNpCX7W/e4yQuVzSnWAeuheb4BcAQSOF3BPVc0jNPitZmkmJDRnZYmY/bw0eMxUsAzVT17mQ21CWm5pj3jRhNSnqGLdDRr8QWJpWnHfv76+K1ugwS/c9PJO8y5KhpOjIZV6CKuu9liJ4kCEmu7F+wa/MxVnrmowm1EVOQl3tRUuMFOp2eBEfGBRyxEzV1fV8rKL4ahdkjVFa8QkNfwZkiRf9S5twQxPtskOHFCqN7G1+Z8aGhMihVA1K1BYDY96EI6+lpzjAPrDb6F74Sn1yz7NxM+3oHphwNITeOuUJZWY3FrPnu3GWUluWKHweT1np5BSwaK0eZmemPt7bsO6oYRV4TDz0urXKeIaUvVt2lc5/gx8a7l5sQM+bPG3f3dndF1VQsn9PFT8ypn8+wrTdz8cvPIPPKIXHSLd+LOCagP/lwnd9O8itjOxxbhSdq1oT9bfzwiSdi08mB4oPEdwXtsFa4WBNuvwn4kulykqBu8id3mSbPDQO2XXrxdMBdrOgfFt9A4NHquGr6ylL9LNcXBxeIzdesUMx/dvx4Ga5Mze5vIYP7mK4vhxs1iXaPDjQ+BcuThX0joe+oIxe7f/fvnmFwAAAP//
+// DO NOT EDIT
+package com.paypal.sdk.paymentexperience;
+
+import com.braintreepayments.http.*;
+import java.util.*;
+import java.util.stream.Collectors;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+/**
+ * Partially-updates a web experience profile, by ID. In the JSON request body, specify a patch object, the path of the profile location to update, and a new value.
+ */
+public class WebProfilePartialUpdateRequest extends HttpRequest<Void> {
+
+    public WebProfilePartialUpdateRequest(String profileId) {
+        super("/v1/payment-experience/web-profiles/{profile_id}?", "PATCH", Void.class);
+        try {
+            path(path().replace("{profile_id}", URLEncoder.encode(String.valueOf(profileId), "UTF-8")));
+        } catch (UnsupportedEncodingException ignored) {}
+        
+        header("Content-Type", "application/json");
+    }
+    
+
+    public WebProfilePartialUpdateRequest requestBody(List<JsonPatch> patchRequest) {
+        super.requestBody(patchRequest);
+        return this;
+    }
+}
